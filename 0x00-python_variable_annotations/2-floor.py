8e0cc0b286b2d1c88e5aba59e2c92208581b4b10
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-11 10:56:49 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Basic annotations - floor"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the given float.
+
+    Parameters:
+    n (float): The input float.
+
+    Returns:
+    int: The floor of the input float.
+    """
+    return math.floor(n)
