2203b4d278f8c87af2c819a5ba9efbdd46d5bb60
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-11 10:23:29 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Concatenates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
