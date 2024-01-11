72922aba12988fc228844e003b87ed546b8794e5
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-11 12:16:49 +0000

    102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """Type Checking Update"""
 
+
 from typing import Tuple, Union, List, Any
 
 
 def zoom_array(lst: Tuple, factor: Union[int, float] = 2) -> Tuple:
+    """Use mypy to validate the following piece of code
+    and apply any necessary changes.
+    """	
     zoomed_in: List[Any] = [
         item for item in lst
         for i in range(int(factor))
