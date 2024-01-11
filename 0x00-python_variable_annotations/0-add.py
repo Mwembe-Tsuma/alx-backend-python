6d7e6eff8a378124227dc891abd2d91e57d48914
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-11 10:21:57 +0000

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Add two flaoting numbers"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns their sum.
+
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    sum of a and b.
+    """
+    return a + b
