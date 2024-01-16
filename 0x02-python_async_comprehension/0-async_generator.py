97bc86bfd3e7e4e9c9abbccfd2876b169c44525f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-16 16:23:38 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,11 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Implementation of the async_generator coroutine"""
-    for _ in range(10):
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
