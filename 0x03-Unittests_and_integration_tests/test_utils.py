134a2b23784799ebfbe869de17087c41c797a3a9
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2024-02-05 13:56:48 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -58,9 +58,9 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """Tests the `memoize` function."""
+    """Tests the memoize function."""
     def test_memoize(self) -> None:
-        """Tests `memoize`'s output."""
+        """Tests memoize results."""
         class TestClass:
             def a_method(self):
                 return 42
@@ -72,8 +72,8 @@ class TestMemoize(unittest.TestCase):
                 TestClass,
                 "a_method",
                 return_value=lambda: 42,
-                ) as memo_fxn:
+                ) as mock_a_method:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
-            memo_fxn.assert_called_once()
+            mock_a_method.assert_called_once()
