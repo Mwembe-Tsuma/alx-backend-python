6afb284509b94892ab5ab304f0511ea8571e13be
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-05 08:36:33 +0000

    test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Test utils module"""
+
+
+import unittest
+from typing import Dict, Tuple, Union
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(unittest.TestCase):
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_results):
+        self.assertEqual(access_nested_map(nested_map, path), expected_results)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, exception):
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
